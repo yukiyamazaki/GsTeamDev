23aae304be2df21c1a4aa1090adcd8332190cf1e
kouichi <isikic.141501@gmail.com>
2020-12-16 16:35:09 +0900

    共通デザイン設定(layouts/app.blade.php)

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
 
 <h1>テストだよ〜〜〜</h1>
 
 <img src="{{ asset('assets/img/test.jpg') }}" alt="">
 
 <button>Click</button>
-    
-<script src="{{ asset('assets/js/app.js') }}"></script>
-</body>
-</html>
+
+@endsection
