0ac41e2a7f1aaa6b8576e221c84dc1eaa5229cf0
kouichi <isikic.141501@gmail.com>
2020-12-16 15:51:55 +0900

    トップページ準備

---
+++ resources/views/top.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+</head>
+<body>
+
+<h1>テストだよ〜〜〜</h1>
+
+<img src="{{ asset('assets/img/test.jpg') }}" alt="">
+
+<button>Click</button>
+    
+<script src="{{ asset('assets/js/app.js') }}"></script>
+</body>
+</html>
