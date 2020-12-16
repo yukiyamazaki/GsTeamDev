3e12c74c2b988018fa73f68a80b24b61f04e78a5
maruha <hariurihapyahoo.co.jp>
2020-12-16 18:50:10 +0900

    針生テスト２
    タイトルをhariuテスト2に変更

--- resources/views/hariu.blade.php
+++ resources/views/hariu.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>hariuテスト</title>
+  <title>hariuテスト2</title>
 </head>
 <body>
   <div>
