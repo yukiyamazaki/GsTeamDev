7773efc961cffc0d37b753327bad15210c09051e
YUKI YAMAZAKI <gooo0523@gmail.com>
2020-12-16 17:07:09 +0900

    usre_table  列名を作成

--- database/migrations/2020_12_16_080207_create_users_table.php
+++ database/migrations/2020_12_16_080207_create_users_table.php
@@ -4,28 +4,39 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+class CreateUserTableTable extends Migration
+        {
+            /**
+             * Run the migrations.
+             *
+             * @return void
+             */
+            public function up()
+            {
+                Schema::create("user_table", function (Blueprint $table) {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
-}
+						$table->bigIncrements('id');
+						$table->string('mail',128);
+						$table->string('lpw',128);
+						$table->string('user_name',128)->nullable();
+						$table->string('nickname',128)->nullable();
+						$table->string('school_category',50)->nullable();
+						$table->integer('gender')->nullable();
+						$table->string('subject',50)->nullable();
+						$table->integer('age')->nullable();
+						$table->string('introduce',255)->nullable();
+						$table->string('image_name',128)->nullable();
+
+                });
+            }
+
+            /**
+             * Reverse the migrations.
+             *
+             * @return void
+             */
+            public function down()
+            {
+                Schema::dropIfExists("user_table");
+            }
+        }
