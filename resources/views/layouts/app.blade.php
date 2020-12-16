216d19722f9ed4d745acc5ac0a63f2879a994504
kouichi <isikic.141501@gmail.com>
2020-12-16 17:16:30 +0900

    ヘッダー／フッターCSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,20 +6,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/destyle.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
 </head>
 
 <body>
 
     <header>
+
         <ul>
             <li>home</li>
             <li>about</li>
             <li>検索</li>
+        </ul>
+
+        <ul>
             <li>投稿</li>
             <li>登録</li>
             <li>アイコン</li>
         </ul>
+
     </header>
 
     <main>
