3a9c27159dc6d8d04665f173985b267ad19e3c66
YUKI YAMAZAKI <gooo0523@gmail.com>
2020-12-16 17:14:38 +0900

    残りのテーブル作成
    post / like /contactテーブル作成

---
+++ database/migrations/2020_12_16_081238_create_contacts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+            {
+                Schema::create("contact_table", function (Blueprint $table) {
+
+						$table->bigIncrements('id');
+						$table->string('user_name',128)->nullable();
+						$table->string('user_mail',128);
+						$table->string('contact_category',128)->nullable();
+						$table->string('contact_content',288);
+						$table->integer('login_user_id')->nullable();
+
+                });
+            }
+
+            /**
+             * Reverse the migrations.
+             *
+             * @return void
+             */
+            public function down()
+            {
+                Schema::dropIfExists("contact_table");
+            }
+        }
