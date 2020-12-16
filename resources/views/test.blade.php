08b86c199742e75f25458bf341754f7ea72dee89
YUKI YAMAZAKI <gooo0523@gmail.com>
2020-12-16 17:46:39 +0900

    test bladeページ作成

---
+++ resources/views/test.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>こちらテストページ</title>
+</head>
+<body>
+  <div>
+    <h1>こちらテストページ</h1>
+  </div>
+
+  <div>
+    <h2>test1 資料投稿FORM</h2>
+    
+  </div>
+  
+</body>
+</html>
