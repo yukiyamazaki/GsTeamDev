0eecdabc34f087a408ffe93b03e16c28468838e3
YUKI YAMAZAKI <gooo0523@gmail.com>
2020-12-20 13:42:05 +0900

    山崎＿route作成

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>共通テンプレート</title>
+</head>
+<body>
+  <main>
+    @yield('content')
+  </main>   
+</body>
+</html>
---
+++ resources/views/logout.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.hp')
+@section('content')
+
+
+@endsection
