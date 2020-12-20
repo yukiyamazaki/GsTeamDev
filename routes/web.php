8f643a394a26219d96d2bd556573652cbb766ff8
YUKI YAMAZAKI <gooo0523@gmail.com>
2020-12-20 14:59:16 +0900

    山崎、ルーティング・コントローラー作成

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,11 @@ Route::get('/', function () {
     return view('top');
 });
 
+//Aboutページへ
+Route::get('/about', function () {
+    return view('about');
+});
+
 //User 新規登録画面へ
 Route::get('/signup', function () {
     return view('signup');
@@ -21,12 +26,12 @@ Route::get('contents/{username}/{contents_id}', function () {
 
 //検索時
 Route::get('/search/{keyword}', function () {
-    return view('/search/{keyword}');
+    return view('search/{keyword}');
 });
 
 //資料投稿ページへ
 Route::get('/upload', function () {
-    return view('/upload');
+    return view('upload');
 });
 
 //マイページへ
@@ -36,14 +41,23 @@ Route::get('myPage/{username}', function () {
    
 //サポートページへ
 Route::get('/suport', function () {
-    return view('/suport');
+    return view('suport');
 });
 
 //ログアウトページへ
 Route::get('/logout', function () {
-    return view('/logout');
+    return view('logout');
 });
 
+//利用規約へ
+Route::get('/userule', function () {
+    return view('userule');
+});
+
+//コンタクトページへ
+Route::get('/contact', function () {
+    return view('contact');
+});
 
 
 
